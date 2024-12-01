6d9474e863391ed13250fdb2775818296ec574b6
oto-webdev <r.otowebdev@gmail.com>
2024-12-02 01:45:14 +0400

    base react setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Routes, Route } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div className='app'>
+      
+    </div>
+  )
+}
+
+export default App;
